89d006e06f393815ff685a49ebfa9db4fb238b6f
OmerutaPro <omeruta@OmerutaPros-MacBook-Pro.local>
2022-11-14 08:24:58 +0100

    memory game completed using useState, useEffect

---
+++ src/components/SingleCard.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import "./SingleCard.css"
+
+const SingleCard = ({card, handleChoice, flipped, disabled}) => {
+
+    const handleClick = () => {
+        if (!disabled) {
+            handleChoice(card)
+        }
+        
+    }
+
+    return ( 
+        <div className='card' >
+          <div className={flipped ? "flipped" : ""}>
+            <img className='front' src={card.src} alt="card front" />
+            <img 
+            className='back' 
+            src="./img/cover.png" 
+            onClick={handleClick}
+            alt="cover" />
+          </div>
+          </div>
+     );
+}
+ 
+export default SingleCard;
